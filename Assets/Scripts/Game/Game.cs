0b3a4a60ce358ba34726bdb22c3c1344ce7a2e86
zhuyunxiang1984 <yunxiang1031@163.com>
2021-09-05 14:04:46 +0800

    [feature] 新增XILRuntime

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -1,15 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using XGameFramework;
+using XGameKit.XILRuntime;
 
 namespace XGameFrameworkHotfix
 {
     public static class Game
     {
-        public static void Start(XILRuntimeDomain domain)
+        static XILRuntime _XilRuntime;
+        public static void Main(XILRuntime xilruntime)
         {
             Debug.Log("Game Start");
+            _XilRuntime = xilruntime;
+            SceneManager.LoadScene("Game");
+        }
+        public static void Exit()
+        {
+            Debug.Log("Game Exit");
+            _XilRuntime = null;
+            SceneManager.LoadScene("Launch");
+        }
+        static float _Time = 1;
+        public static void Tick(float deltaTime)
+        {
+            _Time -= deltaTime;
+            if (_Time <= 0)
+            {
+                _Time = 1f;
+                Debug.Log("Game Tick");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                _XilRuntime.Exit();
+                
+            }
         }
     }
 }
